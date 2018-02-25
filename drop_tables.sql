9a680dc641cd68e9d6ae62df16d6fab449d71e46
Lisa <ssxsora@gmail.com>
2018-02-25 16:42:48 -0500

    Created a script to drop all tables when the tables must be reloaded

---
+++ drop_tables.sql
@@ -0,0 +1,19 @@
+-- -----------------------------------------------------
+-- LW: Drop all tables
+--      This script is used when the DB must be reloaded
+-- -----------------------------------------------------
+
+SET FOREIGN_KEY_CHECKS=0;
+SET UNIQUE_CHECKS=0;
+
+DROP TABLE IF EXISTS `vma`.`patient`;
+DROP TABLE IF EXISTS `vma`.`medicalprocedure`;
+DROP TABLE IF EXISTS `vma`.`appointment`;
+DROP TABLE IF EXISTS `vma`.`postprocedurequestion`;
+DROP TABLE IF EXISTS `vma`.`postprocedurequestionresponse`;
+DROP TABLE IF EXISTS `vma`.`preprocedurequestion`;
+DROP TABLE IF EXISTS `vma`.`preprocedurequestionnumber`;
+DROP TABLE IF EXISTS `vma`.`specialinstruction`;
+
+SET FOREIGN_KEY_CHECKS=1;
+SET UNIQUE_CHECKS=1;
